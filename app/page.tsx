f4e6d71861849a4b4d9e2278bdff88625cf2dc91
santaz1995 <vyacheslav.zheltukhin@gmail.com>
2024-03-12 19:19:06 +0200

    Init

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,10 @@
-import { sql } from '@vercel/postgres';
-import { Card, Title, Text } from '@tremor/react';
-import Search from './search';
-import UsersTable from './table';
+'use client';
+import Stats from './stats';
 
-interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-}
-
-export default async function IndexPage({
-  searchParams
-}: {
-  searchParams: { q: string };
-}) {
-  const users = [] as User[];
-
+export default function IndexPage() {
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      <Title>Users</Title>
-      <Text>A list of users retrieved from a Postgres database.</Text>
-      <Search />
-      <Card className="mt-6">
-        <UsersTable users={users} />
-      </Card>
+      <Stats />
     </main>
   );
 }
